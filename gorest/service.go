fcc45bd28eab6e9028a15cc1313507086eae5bdc
sawadeeeen <sawadeeeen@gmail.com>
2019-02-28 23:55:31 +0900

    [feat]service.goをついかした

---
+++ gorest/service.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"github.com/ant0ine/go-json-rest"
+	"log"
+	"net/http"
+)
+
+type postHelloInput struct {
+	Name string
+}
+
+type postHelloOutput struct {
+	Result string
+}
+
+func postHello(w *rest.ResponseWriter, req *rest.Request) {
+	input := postHelloInput{}
+	err := req.DecodeJsonPayload(&input)
+
+	if err != nil {
+		rest.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if input.Name == "" {
+		rest.Error(w, "Name is required", 400)
+		return
+	}
+
+	log.Printf("%#v", input)
+
+	w.WriteJson(&postHelloOutput{
+		"Hello," + input.Name,
+	})
+}
+
+func main() {
+	handler := rest.ResourceHandler{}
+	handler.SetRoutes(
+		rest.Route("POST", "/hello", postHello),
+	)
+	log.Printf("Server started")
+	http.ListenAndServe(":9999", &handler)
+}
