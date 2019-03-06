624dd8ae6da2ecfed6a13442189d7e24cbfd50b7
sawadeeeen <sawadeeeen@gmail.com>
2019-03-07 02:16:28 +0900

    add comment for finish goroutine

--- goroutine/concurrent.go
+++ goroutine/concurrent.go
@@ -9,35 +9,37 @@ func main() {
 	log.Print("started")
 
 	// channel
-	sleep1_finished := make(chan bool)
-	sleep2_finished := make(chan bool)
-	sleep3_finished := make(chan bool)
+	// sleep1_finished := make(chan bool)
+	// sleep2_finished := make(chan bool)
+	// sleep3_finished := make(chan bool)
+	sleep_finished := make(chan bool)
 
 	go func() {
 		log.Print("sllep1 started ")
 		time.Sleep(1 * time.Second)
 		log.Print("sleep1 finished")
-		sleep1_finished <- true
+		sleep_finished <- true
 	}()
 
 	go func() {
 		log.Print("sleep2 started")
-		time.Sleep(1 * time.Second)
+		time.Sleep(2 * time.Second)
 		log.Print("sleep2 finished")
-		sleep2_finished <- true
+		sleep_finished <- true
 	}()
 
 	go func() {
-		// 3秒かかるコマンド
+		// command taking three minutes
 		log.Print("sleep3 started.")
 		time.Sleep(3 * time.Second)
 		log.Print("sleep3 finished.")
-		sleep3_finished <- true
+		sleep_finished <- true
 	}()
 
-	<-sleep1_finished
-	<-sleep2_finished
-	<-sleep3_finished
+	// need count for concurrent function
+	<-sleep_finished
+	<-sleep_finished
+	<-sleep_finished
 
 	log.Print("all finished")
 
